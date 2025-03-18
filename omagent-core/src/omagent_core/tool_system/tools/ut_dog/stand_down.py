5869ee02567cbf2f074ee4d16b60989700224ff9
潇陵 <347090563@qq.com>
2025-03-18 14:42:26 +0800

    Add utility classes for go2 operations: free avoid, stand up, stand down, move, stop move
    Add utility classes for go2 operations: free avoid, stand up, stand down, move, stop move

---
+++ omagent-core/src/omagent_core/tool_system/tools/ut_dog/stand_down.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+from typing import Any, Dict, Optional, Union
+
+from pydantic import field_validator
+from unitree_sdk2py.core.channel import ChannelSubscriber, ChannelFactoryInitialize
+from unitree_sdk2py.idl.default import unitree_go_msg_dds__SportModeState_
+from unitree_sdk2py.idl.unitree_go.msg.dds_ import SportModeState_
+from unitree_sdk2py.go2.sport.sport_client import (
+    SportClient,
+    PathPoint,
+    SPORT_PATH_POINT_SIZE,
+)
+
+from ....utils.logger import logging
+from ....utils.registry import registry
+from ...base import ArgSchema, BaseTool
+
+CURRENT_PATH = Path(__file__).parents[0]
+
+ARGSCHEMA = {
+}
+
+
+@registry.register_tool()
+class StandDown(BaseTool):
+    """Tool for making Unitree Go2 robot stand down. The robot lies down, and the motor joints remain locked."""
+
+    class Config:
+        """Configuration for this pydantic object."""
+
+        extra = "allow"
+        arbitrary_types_allowed = True
+
+    args_schema: ArgSchema = ArgSchema(**ARGSCHEMA)
+    description: str = "Control the Unitree Go2 robot to stand down. The robot lies down, and the motor joints remain locked."
+    network_interface_name: Optional[str]
+
+    def __init__(self, **data: Any) -> None:
+        super().__init__(**data)
+        ChannelFactoryInitialize(0, self.network_interface_name)
+        self.sport_client = SportClient()  
+        self.sport_client.SetTimeout(10.0)
+        self.sport_client.Init()
+
+    @field_validator("network_interface_name")
+    @classmethod
+    def network_interface_name_validator(cls, network_interface_name: Union[str, None]) -> Union[str, None]:
+        if network_interface_name == None:
+            raise ValueError("network interface name is not provided.")
+        return network_interface_name
+
+    def _run(
+        self
+    ) -> Dict[str, Any]:
+        """
+        Control the Go2 to stand down.
+        """
+
+        try:
+            self.sport_client.StandDown()
+            return {
+                "code": 0,
+                "msg": "success",
+            }
+        except Exception as e:
+            logging.error(f"Stand down failed: {e}")
+            return {
+                "code": 500,
+                "msg": "failed",
+            }
+
+if __name__ == "__main__":
+    tool = StandDown(network_interface_name="eth0")
+    tool.run()
